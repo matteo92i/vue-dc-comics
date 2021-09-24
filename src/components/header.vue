260bdae774afa8feec4fec74b6e25272e0513786
Matteo92i <matteo.iovane@gmail.com>
2021-09-24 17:21:29 +0200

    lista header

--- src/components/header.vue
+++ src/components/header.vue
@@ -5,6 +5,53 @@
 
 <script>
 export default {
+  name:'Header',
+  data: function (){
+    return {
+      linkHeader: [
+        {
+          text:'charater',
+          link: '#charater',
+        },
+        {
+          text:'comics',
+          link: '#comics',
+        },
+                {
+          text:'movies',
+          link: '#movies',
+        },
+        {
+          text:'tv',
+          link: '#tv',
+        },
+                {
+          text:'games',
+          link: '#games',
+        },
+        {
+          text:'collections',
+          link: '#collections',
+        },
+                {
+          text:'video',
+          link: '#video',
+        },
+        {
+          text:'fans',
+          link: '#fans',
+        },
+                {
+          text:'news',
+          link: '#news',
+        },
+        {
+          text:'shop',
+          link: '#shop',
+        },
+      ]
+    }
+  }
     
 }
 </script>
